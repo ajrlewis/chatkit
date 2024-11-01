7aad446098e697cb6669388ddb9214acc133df14
ajrlewis <hello@ajrlewis.com>
2024-11-01 17:33:23 +0000

    Added ask prompt template.

--- src/chatkit/prompt_templates.py
+++ src/chatkit/prompt_templates.py
@@ -2,15 +2,16 @@ from typing import Optional
 
 from loguru import logger
 
+ask = "{text}"
+extract = "Extract (or infer) these data points: `{data_points}` from the following text: `{text}`. If you're not able to extract a data point, return '' for it's value. Finally, return only the extracted data points as a JSON object in your response. Do not format your response in any other way."
 summarize = "Summarize the following text: `{text}`. Return only the summary of the text in your response."
 sentiment = "Determine the sentiment from the following text: `{text}`. Return only 'positive', 'negative' or 'neutral' depending on the sentiment. Do not include any other text."
-extract = "Extract (or infer) these data points: `{data_points}` from the following text: `{text}`. If you're not able to extract a data point, return '' for it's value. Finally, return only the extracted data points as a JSON object in your response. Do not format your response in any other way."
 code = "Generate code in the following language `{language}` to do the following: `{description}`. Return only the code in your response. Do not include any other text."
+
 slogan = "Generate a slogan for company called `{name} ({description})`. Return only the slogan in your response."
 paragraph = "Generate a website paragraph for company called `{name} ({description})`. Return only the paragraph in your response."
 condense = "Analyze this text: `{text}`. Condense the text into a maximum of {number_of_words} words. Return only the condensed text in your response."
 keywords = ""
-ask = ""
 language = ""
 translate = ""
 rewrite = ""
